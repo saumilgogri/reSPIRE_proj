17e2ad5c1c5fcc23692838b0ed514166682500c7
poojan1995 <64237883+poojan1995@users.noreply.github.com>
2020-06-14 13:19:54 -0700

    bava_test_2

--- maulik_test/maulik_test.ino
+++ maulik_test/maulik_test.ino
@@ -10,10 +10,20 @@ servoright.attach(9);  // attaches the servo on pin 9 to the servo object
 void loop() {
   // put your main code here, to run repeatedly:
   int pos;
-  for(pos = 30; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees
+  for(pos = 0; pos <= 90; pos += 1) // goes from 0 degrees to 180 degrees
     {                                  // in steps of 1 degree
       servoright.write(pos);
-      delay(100);                       
+      delay(10);                       
+  
+      // ============ Update pressure valu
+      //Serial.println(pressure_diff);
+      //Serial.println(pressure_expiration);
+    }
+
+   for(pos = 90; pos >= 0; pos -= 1) // goes from 0 degrees to 180 degrees
+    {                                  // in steps of 1 degree
+      servoright.write(pos);
+      delay(10);                       
   
       // ============ Update pressure valu
       //Serial.println(pressure_diff);
